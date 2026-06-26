997e414d20bb164481757741a665fc7d3cc9f27b
Mac_Mac <331863565@qq.com>
2026-06-26 10:59:22 +0800

    feat: add seo meta sitemap and robots

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  integrations: [sitemap()],
   server: {
     host: true,
     port: 4321
